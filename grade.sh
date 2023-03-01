ddbfd366831cd7585cc4cd7daca1e71ee93e9ba6
anl059 <122491210+anl059@users.noreply.github.com>
2023-03-01 13:11:50 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -1,5 +1,27 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm -rf temp
 git clone $1 student-submission
 echo 'Finished cloning'
+if [[ -f student-submission/ListExamples.java ]]
+    then echo "ListExamples found"
+else 
+    echo "Error: ListExamples not found"
+fi
+mkdir temp
+cp student-submission/ListExamples.java temp
+cp TestListExamples.java temp
+cp -r lib temp
+cd temp
+javac -cp $CPATH *.java 2> error.txt
+if [[ $? -ne 0 ]]
+    then echo "Error: Files not compiling"
+    exit 1
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > error.txt
+if grep -q "FAILURES!!!" error.txt
+    then echo "Failed, check error.txt for more details"
+else 
+    echo "Passed"
+fi
